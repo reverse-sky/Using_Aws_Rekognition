3bc20db78d4a31d2493684b1f9cf1370ca69845b
reversesky <45085563+reversesky@users.noreply.github.com>
2021-12-05 21:13:43 +0900

    upload_부분 수정

--- make_polly.py
+++ make_polly.py
@@ -21,4 +21,7 @@ def make_polly(photo, bucket,file_name):
         file = open(f'{file_name}.mp3','wb')
         file.write(response2['AudioStream'].read())
         file.close()
+        mys3 = boto3.client('s3')
+        mys3.upload_file(f'/home/ubuntu/.aws/{file_name}.mp3',bucket,f'file_name'.mp3)
+
         return f"Audio output: {file_name}"
